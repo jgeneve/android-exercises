97a560c0136c8ac1fdf8af03d5051c2ae508abb4
Michaël Ohayon <mohayon75@gmail.com>
2019-11-06 19:51:42 +0100

    Bump gradle and migration to Android X

--- app/src/main/java/fr/android/androidexercises/BookActivity.kt
+++ app/src/main/java/fr/android/androidexercises/BookActivity.kt
@@ -1,7 +1,7 @@
 package fr.android.androidexercises
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 
 class BookActivity : AppCompatActivity() {
